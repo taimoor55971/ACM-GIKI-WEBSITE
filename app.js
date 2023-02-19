f4e47f40b072124db441c7deb51fab3367a3176e
Muhammad Taimoor Khan <62849524+taimoor55971@users.noreply.github.com>
2023-02-19 18:48:41 +0500

    deploying on Adaptable and netlify

--- app.js
+++ app.js
@@ -12,25 +12,7 @@ const path = require("path");
 
 app.use(require("./Router/auth"));
 const PORT = process.env.PORT || 3000;
-// app.get("/", (req, res) => {
-//     res.send("hello to backend");
-// })
- 
 
-// if ( process.env.NODE_ENV == "production"){
-
-//     app.use(express.static("acm/build"));
-
-//     const path = require("path");
-
-//     app.get("*", (req, res) => {
-
-//         res.sendFile(path.resolve(__dirname, 'acm', 'build', 'index.html'));
-
-//     })
-
-
-//}
 
 
 if (process.env.NODE_ENV === "production") {
