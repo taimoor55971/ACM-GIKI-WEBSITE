be86077897ba980a9797cae61593a4fa23be75fe
Muhammad Taimoor Khan <62849524+taimoor55971@users.noreply.github.com>
2023-06-04 04:25:51 +0500

    for one last and final commit

--- acm/src/Components/Gallery/Gallery.jsx
+++ acm/src/Components/Gallery/Gallery.jsx
@@ -1,172 +1,123 @@
-/* eslint-disable no-unused-vars */
-import React, { useEffect, useLayoutEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import 'react-medium-image-zoom/dist/styles.css'
 import Zoom from 'react-medium-image-zoom'
 import "./gallery.css"
 import { Helmet } from "react-helmet";
 import ScrollToTop from "react-scroll-to-top";
 
-const renderData = (items, e) => {
-  
+const Card = ({ item }) => {
   return (
-    
-    <div className="container gallery">
-      <ScrollToTop smooth color="#6f00ff" />
-         <Helmet >
-          <meta charSet="utf-8" />
-          <title>ACM_GIKI Chapter|Gallery</title>
-        <meta name="description" content="ACM GIKI Gallery Page" />
-        <meta name="viewport" content="width=device-width,  initial-scale=1.0 ,user-scalable=no"></meta>
-
-        </Helmet>
-      <div className="row m-2">
-        {items.map((items) => {
-          return (
-            <div key={items.id} className="col-sm-6 col-md-4 v my-2">
-              
-                 <Zoom zoomMargin={15} overlayBgColorEnd='rgba(0, 0, 0, 0.7)'>
-                
-                <img src={items.imageURL} class="figure-img img-fluid rounded" alt={items.comment}/>
-                  </Zoom>
-               
-            </div>
-          );
-        })}
-      </div>
-      </div>
+    <div key={item.id} className="col-sm-6 col-md-4 my-2">
+      <Zoom zoomMargin={15} overlayBgColorEnd='rgba(0, 0, 0, 0.7)'>
+        <img src={item.imageURL} className="figure-img img-fluid rounded" alt={item.comment} />
+      </Zoom>
+    </div>
   );
 };
 
-function Gallery() {
+const Gallery = () => {
   const [data, setData] = useState([]);
-
-  const [currentPage, setcurrentPage] = useState(1);
-  const [itemsPerPage, setitemsPerPage] = useState(12);
-
-  const [pageNumberLimit] = useState(5);
-  const [maxPageNumberLimit, setmaxPageNumberLimit] = useState(5);
-  const [minPageNumberLimit, setminPageNumberLimit] = useState(0);
-  const [Loading, SetLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(12);
+  const [Loading, setLoading] = useState(false);
 
   const handleClick = (event) => {
-    setcurrentPage(Number(event.target.id));
+    setCurrentPage(Number(event.target.id));
   };
 
-  const pages = [];
-  for (let i = 1; i <= Math.ceil(data.length / itemsPerPage); i++) {
-    pages.push(i);
-  }
-
+  const pages = Math.ceil(data.length / itemsPerPage);
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
   const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
 
-  // eslint-disable-next-line no-unused-vars
-  const renderPageNumbers = pages.map((number) => {
-    if (number < maxPageNumberLimit + 1 && number > minPageNumberLimit) {
-      return (
-        <li
-          key={number}
-          id={number}
-          onClick={handleClick}
-          className={currentPage === number ? "active" : null}
-        >
-          {number}
-        </li>
-      );
-    } else {
-      return null;
-    }
+  const renderPageNumbers = Array.from(Array(pages).keys()).map((number) => {
+    const pageNumber = number + 1;
+    return (
+      <li
+        key={pageNumber}
+        id={pageNumber}
+        onClick={handleClick}
+        className={currentPage === pageNumber ? "active" : null}
+      >
+        {pageNumber}
+      </li>
+    );
   });
 
-  useLayoutEffect(() => {
-   loadMessage();
+  useEffect(() => {
+    loadMessage();
   }, []);
 
-  //
   const loadMessage = async () => {
     try {
-      SetLoading(true);
-      fetch("https://acm-giki-webapp.onrender.com/images")
-      .then((response) => response.json())
-      .then((json) => setData(json));
-      SetLoading(false);
+      setLoading(true);
+      const response = await fetch("/images");
+      const json = await response.json();
+      setData(json);
+      setLoading(false);
     } catch (e) {
-     
-      SetLoading(false);
+      setLoading(false);
     }
   };
 
   if (Loading) {
     return 'Loading...';
   }
-  //
 
   const handleNextbtn = () => {
-    setcurrentPage(currentPage + 1);
-
-    if (currentPage + 1 > maxPageNumberLimit) {
-      setmaxPageNumberLimit(maxPageNumberLimit + pageNumberLimit);
-      setminPageNumberLimit(minPageNumberLimit + pageNumberLimit);
-    }
+    setCurrentPage(currentPage + 1);
   };
 
   const handlePrevbtn = () => {
-    setcurrentPage(currentPage - 1);
-
-    if ((currentPage - 1) % pageNumberLimit === 0) {
-      setmaxPageNumberLimit(maxPageNumberLimit - pageNumberLimit);
-      setminPageNumberLimit(minPageNumberLimit - pageNumberLimit);
-    }
+    setCurrentPage(currentPage - 1);
   };
-
-  let pageIncrementBtn = null;
-  if (pages.length > maxPageNumberLimit) {
-    pageIncrementBtn = <li onClick={handleNextbtn}> &hellip; </li>;
-  }
-
-  let pageDecrementBtn = null;
-  if (minPageNumberLimit >= 1) {
-    pageDecrementBtn = <li onClick={handlePrevbtn}> &hellip; </li>;
-  }
 
   const handleLoadMore = () => {
-    setitemsPerPage(itemsPerPage + 12);
+    setItemsPerPage(itemsPerPage + 12);
   };
+
+  const handleLoadLess = () => {
+    setItemsPerPage(itemsPerPage - 12);
+  };
+
+  const showLoadMoreButton = currentPage < pages;
+  const showLoadLessButton = itemsPerPage > 12;
 
   return (
     <>
-     
-      {renderData(currentItems)}
-      {/* <ul className="pageNumbers">
-        <li>
-          <button
-            onClick={handlePrevbtn}
-            disabled={currentPage === pages[0] ? true : false}
-          >
-            Prev
-          </button>
-        </li>
-        {pageDecrementBtn}
-        {renderPageNumbers}
-        {pageIncrementBtn}
-
-        <li>
-          <button
-            onClick={handleNextbtn}
-            disabled={currentPage === pages[pages.length - 1] ? true : false}
-          >
-            Next
-          </button>
-        </li>
-      </ul>
-       */}
-        
-      <div className="">
-      <button onClick={handleLoadMore} className="loadmore btn btn-primary d-flex justify-content-center  "   disabled={currentPage === pages[pages.length - 1] ? true : false}>
-        Load More
-        </button>
+      <div className="container gallery">
+        <ScrollToTop smooth color="#6f00ff" />
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>ACM_GIKI Chapter|Gallery</title>
+          <meta name="description" content="ACM GIKI Gallery Page" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
+        </Helmet>
+        <div className="row m-2">
+          {currentItems.map((item) => (
+            <Card key={item.id} item={item} />
+          ))}
         </div>
+      </div>
+      <div className="d-flex justify-content-center mt-3">
+        {showLoadMoreButton && (
+          <button
+            onClick={handleLoadMore}
+            className="loadmore btn btn-primary"
+            style={{ marginRight: '10px' }}
+          >
+            Load More
+          </button>
+        )}
+        {showLoadLessButton && (
+          <button
+            onClick={handleLoadLess}
+            className="loadmore btn btn-primary"
+          >
+            Load Less
+          </button>
+        )}
+      </div>
     </>
   );
 }
--- acm/src/Components/Team/Member.jsx
+++ acm/src/Components/Team/Member.jsx
@@ -1,154 +1,75 @@
-
-import { React, lazy } from 'react'
-
-
-import "./team.css"
-import { Helmet } from 'react-helmet'
-
+import React from 'react';
+import "./team.css";
+import { Helmet } from 'react-helmet';
 import ScrollToTop from "react-scroll-to-top";
-// const T30=lazy(()=>import("./T30"))
-import T30 from './T30'
-
-
-
-
+import T30 from './T30';
 
 function Member() {
-
-    return (
-      <div id="member">
-        <ScrollToTop smooth color="#6f00ff" />
-        <Helmet>
-          <meta charSet="utf-8" />
-          <title>ACM_GIKI Chapter | OUR Team</title>
-          <meta name="description" content=" ACM GIKI TEAM " />
-          <meta
-            name="viewport"
-            content="width=device-width,  initial-scale=1.0 ,user-scalable=no"
-          ></meta>
-        </Helmet>
-
-        <div>
-          <div className="container">
-            <h2>OUR TEAM</h2>
-            <br />
-            <navbar className="team">
-              <div className="nav" id="nav-tab" role="tablist">
-                <a
-                  className="nav-item nav-link active"
-                  id="nav-home-tab"
-                  data-toggle="tab"
-                  href="#nav-EC"
-                  role="tab"
-                  aria-controls="nav-EC"
-                  aria-selected="false"
-                >
-                  EXECUTIVE COUNCIL
-                </a>
-                <a
-                  className="nav-item nav-link"
-                  id="nav-profile-tab"
-                  data-toggle="tab"
-                  href="#nav-H"
-                  role="tab"
-                  aria-controls="nav-H"
-                  aria-selected="false"
-                >
-                  HEADS
-                </a>
-                <a
-                  className="nav-item nav-link"
-                  id="nav-contact-tab"
-                  data-toggle="tab"
-                  href="#nav-SH"
-                  role="tab"
-                  aria-controls="nav-M"
-                  aria-selected="false"
-                >
-                  SUBHEADS
-                </a>
-                <a
-                  className="nav-item nav-link"
-                  id="nav-contact-tab"
-                  data-toggle="tab"
-                  href="#nav-M"
-                  role="tab"
-                  aria-controls="nav-M"
-                  aria-selected="false"
-                >
-                  MEMBERS
-                </a>
-              </div>
-            </navbar>
-            <hr id="navline" />
-            <div className="tab-content" id="nav-tabContent">
-              <div
-                className="tab-pane fade"
-                id="nav-EC"
-                role="tabpanel"
-                aria-labelledby="nav-profile-tab"
-              >
-                <div className="container">
-                  <h2 style={{ textAlign: "center" }}>EC</h2>
-                  <div
-                    className="row justify-content-md-center"
-                    style={{ height: "100%", width: "100%" }}
-                    id="mem-row"
-                  >
-                    <T30 API={"https://acm-giki-webapp.onrender.com/members29"} />
-                    
-
-                  </div>
+  return (
+    <div id="member">
+      <ScrollToTop smooth color="#6f00ff" />
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>ACM_GIKI Chapter | OUR Team</title>
+        <meta name="description" content="ACM GIKI TEAM" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
+      </Helmet>
+      <div className="container">
+        <h2>OUR TEAM</h2>
+        <br />
+        <div className="team">
+          <ul className="nav nav-tabs nav-fill">
+            <li className="nav-item">
+              <a className="nav-link active" data-toggle="tab" href="#ec">EXECUTIVE COUNCIL</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" data-toggle="tab" href="#heads">HEADS</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" data-toggle="tab" href="#subheads">SUBHEADS</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" data-toggle="tab" href="#members">MEMBERS</a>
+            </li>
+          </ul>
+          <div className="tab-content">
+            <div className="tab-pane fade show active" id="ec">
+              <div className="container">
+                <h2 className="text-center">EC</h2>
+                <div className="row justify-content-center" id="mem-row">
+                  <T30 API={"/members29"} />
                 </div>
               </div>
-              <div
-                className="tab-pane fade"
-                id="nav-H"
-                role="tabpanel"
-                aria-labelledby="nav-contact-tab"
-              >
-                <div className="container">
-                  <h2 style={{ textAlign: "center" }}>BATCH 30</h2>
-                  <div className="row justify-content-md-center" id="mem-row">
-                    <T30 API={"https://acm-giki-webapp.onrender.com/members"} />
-                  </div>
+            </div>
+            <div className="tab-pane fade" id="heads">
+              <div className="container">
+                <h2 className="text-center">HEADS</h2>
+                <div className="row justify-content-center" id="heads-row">
+                  <T30 API={"/members"} />
                 </div>
               </div>
-
-              <div
-                className="tab-pane fade"
-                id="nav-SH"
-                role="tabpanel"
-                aria-labelledby="nav-contact-tab"
-              >
-                <div className="container">
-                  <h2 style={{ textAlign: "center" }}>BATCH 31</h2>
-                  <div className="row justify-content-md-center" id="mem-row">
-                    <T30 API={"https://acm-giki-webapp.onrender.com/members31"} />
-                  </div>
+            </div>
+            <div className="tab-pane fade" id="subheads">
+              <div className="container">
+                <h2 className="text-center">SUBHEADS</h2>
+                <div className="row justify-content-center" id="subheads-row">
+                  <T30 API={"/members31"} />
                 </div>
               </div>
-
-              <div
-                className="tab-pane fade"
-                id="nav-SH"
-                role="tabpanel"
-                aria-labelledby="nav-contact-tab"
-              >
-                <div className="container">
-                  <h2 style={{ textAlign: "center" }}>BATCH 32</h2>
-                  <div className="row justify-content-md-center" id="mem-row">
-                    <T30 API={"https://acm-giki-webapp.onrender.com/members31"} />
-                  </div>
+            </div>
+            <div className="tab-pane fade" id="members">
+              <div className="container">
+                <h2 className="text-center">MEMBERS</h2>
+                <div className="row justify-content-center" id="members-row">
+                  <T30 API={"/members32"} />
                 </div>
               </div>
             </div>
           </div>
-          <br />
-          <br />
         </div>
       </div>
-    );
+    </div>
+  );
 }
 
-export default Member
+export default Member;
